7181d865939fcb5e85307548a2785c8665cdf7fa
Olha_Kadeeva <o.kadeeva.n@gmail.com>
2025-05-30 23:30:29 +0200

    added logic in contacts files

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  const newContacts = Array.from({ length: number }, createFakeContact);
+  const updated = [...contacts, ...newContacts];
+  await writeContacts(updated);
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { writeFile } from 'fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts, null, 2);
+  await writeFile(PATH_DB, data, 'utf-8');
+};
